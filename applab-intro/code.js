38f4176217f4e3411fcf8ea874082f6029475edf
Jia <jiaronghu8@gmail.com>
2023-02-01 18:35:09 +0800

    temp

---
+++ applab-intro/code.js
@@ -0,0 +1,34 @@
+onEvent("buttonLeftt1", "click", function() {
+  setScreen("screen3");
+});
+onEvent("buttonRightt1", "click", function(){
+setScreen("screen2");
+});
+
+onEvent("buttonLeft2", "click", function() {
+  setScreen("screen1");
+});
+onEvent("buttonRight2", "click", function(){
+setScreen("screen3");
+});
+
+onEvent("buttonLeft3", "click", function() {
+  setScreen("screen2");
+});
+onEvent("buttonRight3", "click", function(){
+setScreen("screen1");
+});
+
+onEvent("buttonSuccess3", "click", function() {
+  setScreen("screensuccess");
+});
+onEvent("buttonSuccess2", "click", function(){
+setScreen("screensuccess");
+});
+onEvent("buttonSuccess1", "click", function() {
+  setScreen("screensuccess");
+});
+
+onEvent("buttonRestart", "click", function() {
+  setScreen("screen1");
+});
